cacba37587a5ba5d6f502d5c57d670579ef0e60c
matt700395 <matt7003@korea.ac.kr>
2021-08-11 21:52:10 +0900

    first tdd

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hasangwoo\pycharmprojects\django-testing-tutorial\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
